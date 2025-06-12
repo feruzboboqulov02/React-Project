68f472670dbc98cbf241e95ce2f59994819b7a0f
feruzboboqulov02 <feruzboboqulov002@gmail.com>
2025-06-13 02:22:34 +0500

    forms

--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -1,9 +1,39 @@
+import Button from './Button/Button';
+import {useState} from 'react';
+
+
 export default function FeedbackSection(){
+    const [name, setName] = useState('');
+    const [reason, setReason] = useState('bug');
+
+    function handleNameChange(event) {
+        setName(event.target.value);
+    }
     return(
         <section>
             <h3>
                 Обратная связь
             </h3>
+            <form>
+            <label htmlFor="name">Имя</label>
+            <input type="text" id="name" className="control" value={name} onChange={handleNameChange}/> 
+
+            <label htmlFor="reason">Причина</label>
+            <select id="reason" className="control" value={reason} onChange={(event) => setReason(event.target.value)}>
+                <option value="bug">Ошибка</option>
+                <option value="feature">Функция</option>
+                <option value="other">Другое</option>
+            </select>
+
+            <pre>
+                Name: {name}
+                <br />
+                Reason: {reason}
+            </pre>
+
+
+            <Button>Отправить</Button>
+            </form>
         </section>
     )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import logo from '/public/logo-name.svg';
 import './Header.css';
+ 
 export default function Header(){
     const [now,setNow] = useState(new Date());
     setInterval(() => {
