2a39e1283f86b9cb592c8a21cc5583ebb3b63d9f
feruzboboqulov02 <feruzboboqulov002@gmail.com>
2025-06-13 00:07:17 +0500

    Intro section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import TeachingSection from './components/Button/teachingSection';
 import DifferencesSection from './components/Button/DifferencesSection';
+import IntroSection from './components/Button/IntroSection';
 
 export default function App() {
   return (
     <>
       <Header />
       <main>
+        <IntroSection />
         <TeachingSection />
         <DifferencesSection />
       </main>
---
+++ src/components/Button/IntroSection.jsx
@@ -0,0 +1,12 @@
+export default function IntroSection(){
+    return(
+        <section>
+        <h1 className="centered">
+            Result University
+        </h1>
+        <h3 className="centered" style={{color: '#666'}}>
+            Получите профессию frontend-разработчика и постройте карьеру в IT
+        </h3>
+        </section>
+    )
+}
