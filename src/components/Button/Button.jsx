fa710105b44bf37f9a4ce9399e3d41ec5e63146e
feruzboboqulov02 <feruzboboqulov002@gmail.com>
2025-06-13 02:34:52 +0500

    forms

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,11 @@
 import './Button.css';
 
-export default function Button({children,onClick,isActive}) {
+export default function Button({children,isActive, ...props}) {
 
     return (
-    <button className={isActive ? 'button active' : 'button'} onClick={onClick}>
+    <button 
+    {...props}
+    className={isActive ? 'button active' : 'button'} >
         {children}
     </button>
     )    
--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -1,13 +1,16 @@
+import { e } from 'mathjs';
 import Button from './Button/Button';
 import {useState} from 'react';
 
 
 export default function FeedbackSection(){
     const [name, setName] = useState('');
+    const [hasError, setHasError] = useState(false);
     const [reason, setReason] = useState('bug');
 
     function handleNameChange(event) {
         setName(event.target.value);
+        setHasError(event.target.value.trim().length === 0);
     }
     return(
         <section>
@@ -16,10 +19,20 @@ export default function FeedbackSection(){
             </h3>
             <form>
             <label htmlFor="name">Имя</label>
-            <input type="text" id="name" className="control" value={name} onChange={handleNameChange}/> 
+            <input type="text" 
+            id="name" 
+            className="control" 
+            value={name} 
+            style={{
+                border:hasError ? '1px solid red':null,
+            }}
+            onChange={handleNameChange}/> 
 
             <label htmlFor="reason">Причина</label>
-            <select id="reason" className="control" value={reason} onChange={(event) => setReason(event.target.value)}>
+            <select id="reason" 
+            className="control" 
+            value={reason} 
+            onChange={(event) => setReason(event.target.value)}>
                 <option value="bug">Ошибка</option>
                 <option value="feature">Функция</option>
                 <option value="other">Другое</option>
@@ -32,7 +45,7 @@ export default function FeedbackSection(){
             </pre>
 
 
-            <Button>Отправить</Button>
+            <Button disabled={hasError}>Отправить</Button>
             </form>
         </section>
     )
